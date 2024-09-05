b43f960ab86e8e5a88f26319ee91b57c5d78e545
Hamzah SM <hamzahsaiful782@gmail.com>
2024-09-05 15:17:30 +0700

    blad templating, component tailwind, dashboard view

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title> <!-- menerima data dari route , kemudian dikirimkan ke layout -->
+    <p>My Contact</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'title' => 'Home Page',
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'nama' => 'Budi',
+        'title' => 'About',
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('blog', [
+        'title' => 'Blog',
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', [
+        'title' => 'Contact Us',
+    ]);
 });
